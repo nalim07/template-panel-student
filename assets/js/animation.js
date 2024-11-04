34787819b13824dafe5a8040b9cf60fbaa3675c6
nalim07 <alimdulah@gmail.com>
2024-11-04 16:19:25 +0700

    feat: collapse sidebar dan menambahkan halaman learning data

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -2,6 +2,13 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('hidden');
+    
+    // Menyisakan sidebar untuk ikon
+    if (sidebar.classList.contains('hidden')) {
+        sidebar.style.width = '50px'; // Lebar untuk ikon
+    } else {
+        sidebar.style.width = '200px'; // Lebar penuh sidebar
+    }
 }
 
 // Toggle Menu
