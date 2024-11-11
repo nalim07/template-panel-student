4333e96db47a66f9cdb15ec9f3e89665bd0b195c
nalim07 <alimdulah@gmail.com>
2024-11-11 08:02:51 +0700

    feat: perubahan style icon pada switch mode dan penambahan extend color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          light: "#FFF",
+          dark: "#131420",
+        },
+        stabilo: "#94FE0C",
+        navi: "#111A26",
+        "white-99": '#efefef',
+        "blue-link": "#16A0F9",
+      },
+    },
   },
   plugins: [],
 }
