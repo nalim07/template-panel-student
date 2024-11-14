4391f1e372857a7bd1293b7e738de2e7a9d92702
nalim07 <alimdulah@gmail.com>
2024-11-14 17:01:35 +0700

    menambahkan banner ilustrasi (greeting text)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,24 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: "class",
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         background: {
           light: "#FFF",
           dark: "#131420",
         },
-        stabilo: "#94FE0C",
-        navi: "#111A26",
+        "stabilo": '#94FE0C',
+        "navi": '#111A26',
         "white-99": '#efefef',
-        "blue-link": "#16A0F9",
+        "blue-link": '#16A0F9',
+        "ungu": '#6E62E5',
       },
     },
   },
