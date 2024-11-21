4b19b3894a4ff2b38424e10d982cbb9ee8da76bf
nalim07 <alimdulah@gmail.com>
2024-11-21 14:11:13 +0700

    layout: update responsive di semua layar

--- src/js/script.js
+++ src/js/script.js
@@ -66,3 +66,54 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// Toggle Timetable
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+const schedule = {
+    'Monday': [['07:30', 'Informatics']],
+    'Tuesday': [['07:30', 'Music']],
+    'Wednesday': [['07:30', 'Mathematics']],
+    'Thursday': [['07:30', 'Religion']],
+    'Friday': [['07:30', 'PE']]
+};
+let currentDayIndex = 0;
+
+function updateTimetable() {
+    const currentDay = days[currentDayIndex];
+    document.getElementById('currentDay').textContent = currentDay;
+    const timetableBody = document.getElementById('timetableBody');
+    timetableBody.innerHTML = schedule[currentDay].map(([time, subject]) => `
+        <tr class="border-b dark:border-white">
+            <td class="py-2 text-center">${time}</td>
+            <td class="py-2 text-center">${subject}</td>
+        </tr>
+    `).join('');
+}
+
+document.getElementById('prevDay').addEventListener('click', () => {
+    currentDayIndex = (currentDayIndex - 1 + days.length) % days.length;
+    updateTimetable();
+});
+
+document.getElementById('nextDay').addEventListener('click', () => {
+    currentDayIndex = (currentDayIndex + 1) % days.length;
+    updateTimetable();
+});
+
+// Initialize the timetable
+updateTimetable();
+
+// Toggle More Link
+document.addEventListener("DOMContentLoaded", function () {
+  const textContent = document.getElementById("textContent");
+  const moreLink = document.getElementById("moreLink");
+  const fullText = textContent.textContent.trim(); // Simpan teks asli
+  const maxLength = 50; // Batas karakter sebelum memotong
+
+  // Jika teks lebih panjang dari maxLength
+  if (fullText.length > maxLength) {
+      const truncatedText = fullText.substring(0, maxLength).trim() + "...";
+      textContent.textContent = truncatedText; // Potong teks
+      moreLink.style.display = "inline"; // Tampilkan tautan [more]
+  }
+});
+
