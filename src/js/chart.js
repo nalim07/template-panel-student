b2f9c47b8071f6c829936ea3710cabf7737b77bf
nalim07 <alimdulah@gmail.com>
2024-11-22 16:07:43 +0700

    menambahkan package dari flowbite dan perubahan chart

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,16 +1,89 @@
-var optionsTerm = {
-    chart: {
-        type: 'area',
-        height: '100%',
-        toolbar: { show: false }
-    },
-    series: [{
-        name: 'Performance',
-        data: [50, 75, 85, 90]
-    }],
-    xaxis: { categories: ['Term 1', 'Term 2', 'Term 3', 'Term 4'] },
-    fill: { opacity: 0.5 }
-};
 
-var termChart = new ApexCharts(document.querySelector("#termChart"), optionsTerm);
-termChart.render();
+const options = {
+    // set the labels option to true to show the labels on the X and Y axis
+    xaxis: {
+      show: true,
+      categories: ['Term 1', 'Term 2', 'Term 3', 'Term 4'],
+      labels: {
+        show: true,
+        style: {
+          fontFamily: "Inter, sans-serif",
+          cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+        }
+      },
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+    },
+    yaxis: {
+      show: true,
+      labels: {
+        show: true,
+        style: {
+          fontFamily: "Inter, sans-serif",
+          cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+        },
+        formatter: function (value) {
+          return value;
+        }
+      }
+    },
+    series: [
+      {
+        name: "Term",
+        data: [25, 75, 50, 100],
+        color: "#7E3BF2",
+      },
+    ],
+    chart: {
+      sparkline: {
+        enabled: false
+      },
+      height: "100%",
+      width: "100%",
+      type: "area",
+      fontFamily: "Inter, sans-serif",
+      dropShadow: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
+    },
+    tooltip: {
+      enabled: true,
+      x: {
+        show: false,
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        opacityFrom: 0.55,
+        opacityTo: 0,
+        shade: "#1C64F2",
+        gradientToColors: ["#1C64F2"],
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      width: 6,
+    },
+    legend: {
+      show: false
+    },
+    grid: {
+      show: false,
+    },
+    }
+    
+    if (document.getElementById("labels-chart") && typeof ApexCharts !== 'undefined') {
+    const chart = new ApexCharts(document.getElementById("labels-chart"), options);
+    chart.render();
+    }
+    
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   darkMode: "class",
   theme: {
     screens: {
@@ -28,5 +31,9 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')({
+      charts: true,
+    })
+  ],
 }
